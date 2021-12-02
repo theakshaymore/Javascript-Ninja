e212a49793c5b1f93cd4950faf122f2b525f32eb
Akshay More <45824962+theakshaymore@users.noreply.github.com>
2021-12-02 17:30:17 +0530

    ballon d'or winner

---
+++ Ballon d'or/script.js
@@ -0,0 +1,62 @@
+const courses = [
+  {
+    name: " Johan Cruyff",
+    age: "3",
+  },
+  {
+    name: "Cristiano Ronaldo",
+    age: "5",
+  },
+
+  {
+    name: "Franz Beckenbauer",
+    age: "2",
+  },
+  {
+    name: "Lionel Messi",
+    age: "7",
+  },
+  {
+    name: "Ronaldinho",
+    age: "1",
+  },
+  {
+    name: "Ronaldo",
+    age: "2",
+  },
+  {
+    name: " Karl-Heinz Rummenigge",
+    age: "2",
+  },
+];
+
+function generateLIST() {
+  const ul = document.querySelector(".list-group");
+  ul.innerHTML = "";
+  courses.forEach((course) => {
+    const li = document.createElement("li");
+    li.classList.add("list-group-item");
+
+    const name = document.createTextNode(course.name);
+    li.appendChild(name);
+
+    const span = document.createElement("span");
+    span.classList.add("float-right");
+    const age = document.createTextNode(course.age);
+    span.appendChild(age);
+
+    li.appendChild(span);
+    ul.appendChild(li);
+  });
+}
+
+// generateLIST();
+
+window.addEventListener("load", generateLIST);
+
+const button = document.querySelector(".sort-btn");
+
+button.addEventListener("click", () => {
+  courses.sort((a, b) => b.age - a.age);
+  generateLIST();
+});
