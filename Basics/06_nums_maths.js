62ab15755a6fd0bd14aa76be14a7338f370ccc16
Akshay More <45824962+theakshaymore@users.noreply.github.com>
2023-02-28 12:49:38 +0530

    Math

---
+++ Basics/06_nums_maths.js
@@ -0,0 +1,27 @@
+// Number
+
+const number = 777;
+console.log(number);
+
+const balance = new Number(666);
+console.log(balance);
+
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2));
+
+let anotherNumber = 10000000;
+console.log(anotherNumber.toLocaleString());
+console.log(anotherNumber.toLocaleString("en-IN"));
+
+// ---------------------------------------------------------------------
+
+// Maths
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.ceil(5.4));
+console.log(Math.floor(5.4));
+console.log(Math.round(5.4));
+
+console.log(Math.random());
